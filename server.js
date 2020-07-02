27f697cc58d90e4e6a3c99136a08bdebf29fbcbf
Jamey Gronewald <jrgronewald@gmail.com>
2020-07-02 11:14:19 -0700

    updated server with post route

--- server.js
+++ server.js
@@ -2,13 +2,22 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Array variables
+const tables = [
+    {
+        tableNumber: 1,
+        name: 'Betsy',
+        email: 'betsy@gmail.com',
+        phone: 4044044040,
+        id: 5
+    }
+];
 
 // get route for home page
 app.get("/", function(req, res) {
@@ -22,6 +31,17 @@ res.sendFile(path.join(__dirname, "tables.html"));
 app.get("/reserve", function(req, res) {
 res.sendFile(path.join(__dirname, "reserve.html"));
 });
+
+app.get("/api/tables", function(req, res) {
+    return res.json(tables);
+  });
+
+app.post("/api/tables", function(req, res) {
+    let newTable  = req.body;
+    tables.push(newTable);
+    res.json(newTable);
+});
+
 
 app.listen(PORT, function() {
 console.log(`App listening on http://localhost:${PORT}`);
